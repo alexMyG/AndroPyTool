d4789fa52b76dcfee6445d7a85054c0fd26336b3
Arturo Blázquez <arturo.blazquezperez@gmail.com>
2020-11-07 12:03:25 +0100

    Enable CORS

--- rest_api_app.py
+++ rest_api_app.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from flask import Flask
+from flask_cors import CORS
 
 from rest_api.files_controller import app_files
 from rest_api.reports_controller import app_reports
@@ -10,6 +11,7 @@ main_app = Flask(__name__)
 main_app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024  # 32MB
 main_app.register_blueprint(app_reports)
 main_app.register_blueprint(app_files)
+CORS(main_app)
 
 if not os.path.exists(os.path.join("rest_api", 'virus_total_api_key')):
     print "You have to add a default virusTotal API key on file 'rest_api/virus_total_api_key'"
